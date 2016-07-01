770e772ab7e7f9b328f908060ddc65db2d8c8269
soxface <sonyacorcoran@gmail.com>
2016-07-01 11:39:33 +1000

    on level 4

--- sox-beginner/Player.js
+++ sox-beginner/Player.js
@@ -1,13 +1,32 @@
+const MINHEALTH = 8
+const RESTED = 14
+
 class Player {
-  playTurn(warrior) {
-  
-    if (warrior.feel().isEmpty()) {
+  restOrWalk(warrior) {
+    let rested = (warrior.health() >= MINHEALTH) ? true : false;
+
+    if (rested === true) {
       warrior.walk();
-    } else if (warrior.health() < 10) {
-      warrior.rest();
     } else {
-      warrior.attack();
+      warrior.rest();
     }
-    
+  }
+
+  playTurn(warrior) {
+    if (warrior.feel().isEnemy()) {
+      warrior.attack();
+    } else {
+      this.restOrWalk(warrior);
+    }
   }
 }
+
+global.Player = Player;
+
+/*
+> = Stairs
+@ = Warrior (20 HP)
+s = Sludge (12 HP)
+S = Thick Sludge (24 HP)
+C = Captive (1 HP)
+*/
